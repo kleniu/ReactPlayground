bfcb7ae301e55f6963914a107e5809b54243e086
Robert Kleniewski <robert.kleniewski@pl.ibm.com>
2023-04-13 15:45:54 +0200

    final

--- react-playground/02-StylingAppComponents/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-playground/02-StylingAppComponents/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,19 +5,26 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    setIsValid(true);
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length === 0){
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      {/* changing style dynamically */}
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
